ae59400b348aa602d04b629f162a06521e0dfa4c
Pawel L <virtuozoo@o2.pl>
2021-01-18 15:23:54 +0100

    add prettier/eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['airbnb-typescript-prettier'],
+  rules: {
+    'react/prop-types': 0,
+    'react/destructuring-assignment': 0,
+    'react/static-property-placement': 0,
+    'jsx-a11y/alt-text': 0,
+    'react/jsx-props-no-spreading': 0,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+};
